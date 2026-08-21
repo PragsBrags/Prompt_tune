53e83b94c2a2eecbd2a48b62d2e1ab60613f0db7
herog <pragyanofc@gmail.com>
2026-08-21 13:06:31 +0545

    feat: eval pipeline with batch inference and metric calculation for both base and fine tuned models

--- src/cli.py
+++ src/cli.py
@@ -1,26 +1,35 @@
-from evaluation.runner import run_evaluation
-from training.sft import train_model
-from training.save import save_model
-
-from omegaconf import DictConfig, OmegaConf
 import hydra
+from omegaconf import DictConfig
 from transformers import set_seed
 
 
-
-@hydra.main(version_base=None, config_path="../configs", config_name="config")
+@hydra.main(
+    version_base=None,
+    config_path="../configs",
+    config_name="config",
+)
 def main(cfg: DictConfig):
+
     if cfg.run.mode == "evaluate":
+        from evaluation.runner import run_evaluation
+
         set_seed(cfg.run.seed, deterministic=True)
         results = run_evaluation(cfg)
         print(results)
 
-    if cfg.run.mode == "train":
+    elif cfg.run.mode == "train":
+        from training.sft import train_model
+        from training.save import save_model
+
         set_seed(cfg.run.seed, deterministic=True)
+
         model, tokenizer = train_model(cfg)
         print("training complete")
+
         save_model(model, tokenizer, cfg.run)
+
         print("training complete and model saved")
+
 
 if __name__ == "__main__":
     main()
