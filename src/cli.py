6bf416d256673ee1f71ed804fd8d95aca026fabe
herog <pragyanofc@gmail.com>
2026-08-22 20:12:45 +0545

    feat:training and evaluation logger implemented

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,7 @@
 import hydra
 from omegaconf import DictConfig
 from transformers import set_seed
-
+from tracking.experiment import ExpLogger
 
 @hydra.main(
     version_base=None,
@@ -9,24 +9,49 @@ from transformers import set_seed
     config_name="config",
 )
 def main(cfg: DictConfig):
-
+    
     if cfg.run.mode == "evaluate":
         from evaluation.runner import run_evaluation
 
+        logruns = ExpLogger("run_experiments")
         set_seed(cfg.run.seed, deterministic=True)
         results = run_evaluation(cfg)
+
+        logruns.log_run(
+            cfg.model.name,
+            cfg.run.mode,
+            cfg.eval_data.dataset_name,
+            cfg.eval_data.directions[0].dataset_config,
+            None,
+            )
+        
         print(results)
+
+        logruns.log_eval(
+            cfg.model.name,
+            results
+        )
 
     elif cfg.run.mode == "train":
         from training.sft import train_model
         from training.save import save_model
 
+        logruns = ExpLogger("train_experiments")
         set_seed(cfg.run.seed, deterministic=True)
 
         model, tokenizer = train_model(cfg)
+
         print("training complete")
 
         save_model(model, tokenizer, cfg.run)
+
+        logruns.log_run(
+                    cfg.model.name,
+                    cfg.run.mode,
+                    cfg.train_data.dataset_name,
+                    None,
+                    cfg.training,
+                            )
 
         print("training complete and model saved")
 
